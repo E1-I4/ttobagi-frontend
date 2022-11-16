2eee6efe04144c010d3d0449d571b0631502e636
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-11-17 00:32:53 +0900

    :art: 메인 페이지 정리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>test</h1>
+    </div>
+  );
 }
 
 export default App;
