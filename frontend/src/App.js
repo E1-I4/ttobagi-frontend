b01e2340f5c46a6239926057ffed818342d56048
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-11-17 02:07:07 +0900

    :closed_lock_with_key: https 서버 실행

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Album from "./pages/Album";
+import Map from "./pages/Map";
+
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/album" element={<Album />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="*" element={<div>해당 주소는 없는 페이지입니다.</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
