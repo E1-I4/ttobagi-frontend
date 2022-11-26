6094678fc97b50ce614014456a5b21adb11dc0cd
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-26 23:55:36 +0900

    ✨ feat(Login): implement refreshing token

--- frontend/src/pages/KakaoLogin.js
+++ frontend/src/pages/KakaoLogin.js
@@ -1,25 +1,31 @@
-import { useEffect } from "react";
+import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../utils/Urls";
+import { checkAccessToken } from "../utils/Token";
+import { useEffect } from "react";
+
+const JWT_EXPIRE_TIME = 2 * 3600 * 1000; // expiration time(2 hours in milliseconds)
 
 const KakaoLogin = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const KAKAO_CODE = location.search.split("=")[1];
 
-    const BACKEND = "http://localhost:8000";
-
-    console.log(KAKAO_CODE);
-    fetch(`${BACKEND}/api/user/kakao/callback/?code=${KAKAO_CODE}`, {
-        method: "GET",
-    })
-        .then((res) => res.json())
-        .then((res) => {
-            localStorage.setItem("access_token", res.access_token);
-            localStorage.setItem("refresh_token", res.refresh_token);
-            localStorage.setItem("user_id", res.user.pk);
-            navigate("/");
-        });
-
+    useEffect(() => {
+        fetch(`${BACKEND_URL}/api/user/kakao/callback/?code=${KAKAO_CODE}`, {
+            method: "GET",
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                sessionStorage.setItem("access_token", res.access_token);
+                sessionStorage.setItem("refresh_token", res.refresh_token);
+                sessionStorage.setItem("user_id", res.user.pk);
+                axios.defaults.headers.common["Authorization"] =
+                    `"Bearer ${res.access_token}`;
+                setTimeout(checkAccessToken, JWT_EXPIRE_TIME - 60000); // 1 minute before expiration
+                navigate("/");
+            });
+    }, []);
     return <></>;
 };
 
