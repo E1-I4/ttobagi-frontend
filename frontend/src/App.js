d7e6051984455c3442b0128c731ae366fda589a3
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-26 23:54:20 +0900

    ♻️ refactor(LoginAuth): move auth layout to components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import Achieve from "./pages/Achieve";
 import Map from "./pages/Map";
 import Login from "./pages/Login";
 import KakaoLogin from "./pages/KakaoLogin";
-import AuthLayout from "./pages/AuthLayout"
+import AuthLayout from "./components/AuthLayout"
 
 function App() {
   return (
