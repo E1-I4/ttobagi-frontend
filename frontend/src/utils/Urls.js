0c8e90a1426bf2610f52549df08387f9e151acbc
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-11-29 01:49:08 +0900

    :adhesive_bandage: url 수정 1차 끝

--- frontend/src/utils/Urls.js
+++ frontend/src/utils/Urls.js
@@ -1,2 +1,2 @@
-export const REDIRECT_URI = "http://localhost:3000/kakaoLogin";
+export const REDIRECT_URI = "https://www.ttobagi.site/kakaoLogin";
 export const BACKEND_URL = "https://api.ttobagi.site";
