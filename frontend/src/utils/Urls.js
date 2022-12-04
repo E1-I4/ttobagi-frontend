388524320a9dea2818dc8bdcd9d1c3b724aabfa4
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-12-04 20:50:39 +0900

    :lipstick: 로그인 화면 ui 수정

--- frontend/src/utils/Urls.js
+++ frontend/src/utils/Urls.js
@@ -1,2 +1,3 @@
-export const REDIRECT_URI = "https://www.ttobagi.site/kakaoLogin";
+// export const REDIRECT_URI = "https://www.ttobagi.site/kakaoLogin";
+export const REDIRECT_URI = "http://localhost:3000/kakaoLogin";
 export const BACKEND_URL = "https://api.ttobagi.site";
