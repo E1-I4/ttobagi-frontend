5e3d6449b8b203464c2b5a96c37d1eaea67c5592
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-12-06 19:03:13 +0900

    :rocket: v1.0 최종 수정 완료

--- frontend/src/utils/Urls.js
+++ frontend/src/utils/Urls.js
@@ -1,3 +1,5 @@
-// export const REDIRECT_URI = "https://www.ttobagi.site/kakaoLogin";
-export const REDIRECT_URI = "http://localhost:3000/kakaoLogin";
+// local 작업용 url
+// export const REDIRECT_URI = "http://localhost:3000/kakaoLogin";
+
+export const REDIRECT_URI = "https://www.ttobagi.site/kakaoLogin";
 export const BACKEND_URL = "https://api.ttobagi.site";
